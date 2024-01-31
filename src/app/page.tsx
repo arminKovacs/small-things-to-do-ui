fe0905eef372a2db038f69c4a065327780c227a7
Armin Kovacs <armin_kovacs@epam.com>
2024-01-31 10:26:16 +0100

    Use App Router example instead of Pages Router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Link from '../src/Link';
-import ProTip from '../src/ProTip';
-import Copyright from '../src/Copyright';
+import Link from '@mui/material/Link';
+import NextLink from 'next/link';
+import ProTip from '@/components/ProTip';
+import Copyright from '@/components/Copyright';
 
-export default function About() {
+export default function Home() {
   return (
     <Container maxWidth="lg">
       <Box
@@ -20,13 +20,11 @@ export default function About() {
         }}
       >
         <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Material UI - Next.js example in TypeScript
+          Material UI - Next.js App Router example in TypeScript
         </Typography>
-        <Box sx={{ maxWidth: 'sm' }}>
-          <Button variant="contained" component={Link} noLinkStyle href="/">
-            Go to the home page
-          </Button>
-        </Box>
+        <Link href="/about" color="secondary" component={NextLink}>
+          Go to the about page
+        </Link>
         <ProTip />
         <Copyright />
       </Box>
