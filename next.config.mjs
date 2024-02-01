0621f4b842e93ca93b9e858c7ca16d216d4e5452
Armin Kovacs <armin_kovacs@epam.com>
2024-02-01 16:17:27 +0100

    Update for github pages deployment

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: "/small-things-to-do-ui",
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
